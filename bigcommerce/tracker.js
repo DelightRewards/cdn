52ae872bd6ecf0be0f5bea57541b1b694ea8adc1
Beltran Caliz <bjc8008@rit.edu>
2021-08-16 10:39:51 +0200

    Initial commit

---
+++ bigcommerce/tracker.js
@@ -0,0 +1,39 @@
+<script>
+  const sendPayloadToBackend = (data) => {
+  const targetScript = document.querySelector('script[data-store]');
+  if (targetScript){
+    const storeName = targetScript.getAttribute('data-store');
+    const url =
+      "https://us-central1-spherical-gate-286620.cloudfunctions.net/sendReferralDataSomewhere";
+    const response = fetch(url, {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        store: storeName,
+        data: data,
+        source: "delightrewards",
+      }),
+    });
+  }
+
+};
+
+  window.addEventListener("load", () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const utmSource = urlParams.get('utm_source');
+    const userId = localStorage.getItem('');
+
+    if (utmSource && utmSource === "delightrewards" && targetScript){
+      const storeName = targetScript.getAttribute('data-store');
+      if (storeName){
+        sendPayloadToBackend({
+          user: userId || 'no-storage'
+        });
+      }
+    }
+  });
+</script>
